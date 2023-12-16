07d455648b06fb06cec93053319051dc82cb0efc
TrangDinh524 <148752559+TrangDinh524@users.noreply.github.com>
2023-12-17 02:50:26 +0800

    popout ver (chua hoan thien)

--- connect4.py
+++ connect4.py
@@ -1,7 +1,4 @@
 #add them nay:
-#1.neu check_move ma false thi sao -> print nho user chon lai column 
-#2. input la chu?
-#3 neu cot do het avail position -> ask to choose another col
 #4 neu full bang --> end game 
 
 #import necessary libs
@@ -39,31 +36,51 @@ def main():
         turn = turn % 2
 
 def start(game, player):
-    col = int(input(f"Player {player} please pick a column (0-6): "))
-    while not is_valid_input(col):
-        col = int(input(f"Player {player} please pick a column again (0-6): "))
-    if check_move(game.mat, col):
-        row = apply_move(game.mat, col, player)
+    action, col = check_input(game.mat, player)
+    if check_move(game.mat, action, col, player):
+        apply_move(game.mat, action, col, player)
         if check_victory(game.mat, player):
             print(f"Congrats! Player {player} win")
             game.playing = False
 
-def is_valid_input(col):
-    if col not in range(COL_NUM):
-        print("Invalid input. Try again.")
-        return False
-    else:
-        return True 
+def check_input(mat, player):
+    while True:
+        try: 
+            print(f"Player {player}, which mode you want to choose?")
+            action = input(f"\"a\" for add-in, \"r\" for remove: ").lower()
+            if action == "a" or action == "r":
+                while True:
+                    try: 
+                        col = int(input(f"Player {player} please pick a column to add-in (0-6): "))
+                        if col not in range(COL_NUM):
+                            print("Invalid input. Try again.")
+                        elif not check_move(mat, action, col, player):
+                            print("No available position in this column. Please try another col!")    
+                        else:
+                            return action, col
+                    except ValueError:
+                        print("Invalid input. Please enter a valid integer.")
+            else:
+                print("Invalid input. Try again.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
 
 #check wether the highest col still free
-def check_move(mat,col):
-    return mat[ROW_NUM-1][col] == 0
+def check_move(mat, action, col, player):
+    if action == "a":
+        return mat[ROW_NUM-1][col] == 0
+    else:
+        return mat[0][col] == player
 
-def apply_move(mat, col, player):
-    for r in range(ROW_NUM):
-        if mat[r][col] == 0:
-            mat[r][col] = player
-            return r
+def apply_move(mat, action, col, player):
+    if action == "a":
+        for r in range(ROW_NUM):
+            if mat[r][col] == 0:
+                mat[r][col] = player
+                return r
+    else:
+        mat[0][col] = 0
 
 #rules 
 def check_victory(mat, player):
